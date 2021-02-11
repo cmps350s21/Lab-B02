07cde2c54f9949741f1ffa5bf8197d74513490f3
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-11 18:28:00 +0300

    Update excercise1.js

--- Lab 5- JS Fundamentals/Tutorial/excercise1.js
+++ Lab 5- JS Fundamentals/Tutorial/excercise1.js
@@ -11,15 +11,26 @@ let names = ["Ali", "Faisal", "Mohamed"];
 console.log(names)
 //add something to the start
 names.unshift("to the beginning")
-console.log('after unshift' , names)
+console.log('after unshift', names)
 
 names.push('End of the array')
-console.log('after push;' , names)
+console.log('after push;', names)
 
 //remove the last element
 names.pop()
-console.log('after pop' , names)
+console.log('after pop', names)
 
 //remove the last beginning
 names.shift()
-console.log('after shift' , names)
+console.log('after shift', names)
+
+names.splice(2, 1)
+console.log(names)
+
+let x = [1, 2, 3, 4, 5, 6]
+console.log(x.filter(x => x > 4))
+console.log(x.find(x => x == 0))
+
+let w =    [1, [2,5,16], 3, 4, 5, 6].flat(2).reduce()
+w= w.flat(2)
+console.log(w)
