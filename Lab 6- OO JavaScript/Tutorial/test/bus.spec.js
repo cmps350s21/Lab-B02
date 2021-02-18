560ffe0c1e208aec1880e694bb6520f53687bc7e
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-18 17:50:23 +0300

    Update bus.spec.js

--- Lab 6- OO JavaScript/Tutorial/test/bus.spec.js
+++ Lab 6- OO JavaScript/Tutorial/test/bus.spec.js
@@ -3,17 +3,17 @@ const Bus = require('../model/bus');
 
 //test
 describe('Testing the Bus class', ()=>{
-    it('when horse power is <600' , ()=>{
+    it('when horse power is <600 we expect the insurance to be 1000' , ()=>{
         let bus = new Bus('Toyota', 550, 40);
-        expect(bus.getInsurance()).equal(500)
+        expect(bus.getInsurance()).lessThan(700)
     })
 
-    it('when horse power is <1000' , ()=>{
+    it('when horse power is <1000 we expect the insurance to be 1000' , ()=>{
         let bus = new Bus('Toyota', 800, 40);
-        expect(bus.getInsurance()).equal(1000)
+        expect(bus.getInsurance()).lessThan(1200)
     })
 
-    it('when horse power is >=1000' , ()=>{
+    it('when horse power is >=1000 we expect the insurance to be 1000' , ()=>{
         let bus = new Bus('Toyota', 1200, 40);
         expect(bus.getInsurance()).equal(2000)
     })
