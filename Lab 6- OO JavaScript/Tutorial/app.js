6b9e215597f419a4e6b8e5920ed4547788a73d04
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-25 15:43:26 +0300

    lab 7

--- Lab 6- OO JavaScript/Tutorial/app.js
+++ Lab 6- OO JavaScript/Tutorial/app.js
@@ -1,14 +1,19 @@
 const Bus = require('./model/Bus');
 const Truck = require('./model/Truck');
+const ShowRoom = require('./model/ShowRoom');
 
 //create object of a bus
 let bus = new Bus('Toyota XZ125' , 1200 , 50);
 
-let busses = [
+let vehicles = [
     new Bus('Toyota XZ125' , 1200 , 50),
     new Bus('Toyota XZ125' , 1200 , 50),
-    new Bus('Toyota XZ125' , 1200 , 50),
-    new Bus('Toyota XZ125' , 1200 , 50)
-
+    new Truck('Toyota XZ125' , 1200 , 10200),
+    new Truck('Toyota XZ125' , 1200 , 53000),
 ]
+let serialized = JSON.stringify(vehicles)
+console.log(serialized)
 
+let deserialized = JSON.parse(serialized)
+
+console.log(JSON.parse(deserialized))
---
+++ Lab 6- OO JavaScript/Tutorial/model/ShowRoom.js
@@ -0,0 +1,15 @@
+class  ShowRoom{
+    constructor(vehicles){
+        this.vehicles = vehicles;
+    }
+    
+    addVehicle(vehicle){
+        this.vehicles.push(vehicle)
+    }
+
+    updateVehicle(vehicle){
+
+    }
+
+}
+module.exports = ShowRoom;
