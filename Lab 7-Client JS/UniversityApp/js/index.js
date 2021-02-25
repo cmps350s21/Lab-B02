479fcbe753668e545c7004f889ca4ae745b8281b
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-25 17:07:38 +0300

    updated

--- Lab 7-Client JS/UniversityApp/js/index.js
+++ Lab 7-Client JS/UniversityApp/js/index.js
@@ -2,11 +2,31 @@ const searchBar = document.querySelector('#search-bar')
 const universityDD = document.querySelector('#universities')
 const searchBtn = document.querySelector('#search-btn')
 const website = document.querySelector('#website')
+const logo = document.querySelector('#logo')
 
-let fakeData = ['QU' , 'HBKU' , 'Harvard']
+const baseURL = 'http://universities.hipolabs.com/search?country='
 
-searchBtn.onclick = ()=>{
-    fakeData.push(searchBar.value)
-    console.log(fakeData)
+async function getUniversities(url){
+    const data = await fetch(url)  //get you all the data [45] ->Promise
+    const universities = await data.json()
+    loadUniversityDD(universities)
+}
+
+searchBtn.onclick = () => {
+    const url = `${baseURL}${searchBar.value}`
+    getUniversities(url)
+}
+
+universityDD.onchange = ()=>{
+    website.src = universityDD.value
+}
+
+function uniToHTML(uni) {
+    return `<option value="${uni.web_pages[0]}" selected> ${uni.name}</option>`
+}
+
+function loadUniversityDD(universities) {
+    const universitiesHTML = universities.map(uni => uniToHTML(uni)).join('')
+    universityDD.innerHTML = universitiesHTML
 }
 
