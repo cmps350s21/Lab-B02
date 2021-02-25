5e6229af1297a8206c3f68afc6d88acbbd041329
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-02-25 18:43:35 +0300

    Update index.js

--- Lab 7-Client JS/TodoListApp/js/index.js
+++ Lab 7-Client JS/TodoListApp/js/index.js
@@ -25,18 +25,29 @@ function loadTodos() {
 }
 
 function showTodos() {
-    const todosHTML = todos.map(todo => todoToHTMLDiv(todo)).join('')
-    todolistContainer.innerHTML = todosHTML
+    const todosHTML = todos.map(todo => todoToHTMLDiv(todo))
+
+    todolistContainer.innerHTML = todosHTML.join('')
     todoInputBox.value = ''
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 function todoToHTMLDiv(todo) {
     return `<div class="form-group todo" id="${todo.id}">
-                <p class="todo-title" id="title${todo.id}">${todo.title}</p>
-                <input class="completed icon" type="checkbox">
-                <i class="fa fa-trash icon" onclick="deleteTodo(${todo.id})"></i>
-            </div>`;
+                    <p class="todo-title" id="title${todo.id}">${todo.title}</p>
+                    <input class="completed icon" type="checkbox" 
+                    onclick="completed(${todo.id}, this)">
+                    <i class="fa fa-trash icon" onclick="deleteTodo(${todo.id})"></i>
+             </div>`
 }
+
+function completed(todoId, element) {
+    const title = document.querySelector(`#title${todoId}`)
+    if (element.checked)
+        title.classList.add('strike')
+    else
+        title.classList.remove('strike')
+}
+
 
 function deleteTodo(id) {
     console.log(id)
