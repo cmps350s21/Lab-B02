83b42f104de2d094be1b02952fd9340f67820b2a
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-04 17:39:48 +0300

    upto delete

--- Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
@@ -1,23 +1,64 @@
 const countryInputBox = document.querySelector("#country")
 const populationInputBox = document.querySelector("#population")
 const formElement = document.querySelector("#form")
+const countriesTable = document.querySelector("#countries")
 
 formElement.addEventListener('submit', addCensus)
 
-//database
+//database //indexedDB
+//Localbase
+
 const db = new Localbase('b02.census.db')
+showCensusData()
+
+async function deleteCensus(cid) {
+    await db.collection('census').doc({id: cid}).delete()
+    await showCensusData()
+}
+
+function censusToHTMLTRow(c) {
+    return `
+         <tr>
+            <td>${c.country}</td>
+            <td>${c.population}</td>
+            <td>
+                <i class="fa fa-edit">Edit</i>
+                <i class="fa fa-trash" onclick="deleteCensus('${c.id}')">Delete</i>
+            </td>
+        </tr>
+    `
+}
+
+async function showCensusData() {
+    //open the database and read all the doc inside the collection]
+    const census = await db.collection('census').get()
+
+    //we will convert this collection data into html table
+    if (census.length > 0) {
+        const censusHTMLRows = census.map(c => censusToHTMLTRow(c))
+        countriesTable.innerHTML = `
+        <thead> 
+            <tr>
+                <th>Country</th>
+                <th>Population</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        ${censusHTMLRows.join('')}
+    `
+    }
+}
+
 async function addCensus(event) {
     event.preventDefault() //do not reload the page
 
     const newCensus = {
-        id : Date.now().toString(),
+        id: Date.now().toString(),
         country: countryInputBox.value,
         population: populationInputBox.value
     }
-    //order []stop here and wait until we get result
-
     const message = await db.collection('census').add(newCensus)
-    console.log(message)
+    await showCensusData()
 }
 
 
