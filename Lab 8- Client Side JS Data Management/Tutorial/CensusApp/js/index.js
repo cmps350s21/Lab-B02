e05ae131acc5fdaefed0d1982c0968cdd31746ea
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-04 17:01:19 +0300

    Update index.js

--- Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
@@ -10,9 +10,11 @@ async function addCensus(event) {
     event.preventDefault() //do not reload the page
 
     const newCensus = {
+        id : Date.now().toString(),
         country: countryInputBox.value,
         population: populationInputBox.value
     }
+    //order []stop here and wait until we get result
 
     const message = await db.collection('census').add(newCensus)
     console.log(message)
