9c648af23212afc78b29bbdc793ba8bddf44398a
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-04 18:21:32 +0300

    update

--- Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
+++ Lab 8- Client Side JS Data Management/Tutorial/CensusApp/js/index.js
@@ -10,10 +10,48 @@ formElement.addEventListener('submit', addCensus)
 
 const db = new Localbase('b02.census.db')
 showCensusData()
+let censusToEditId;
+let isEdit = false;
+
+function form2Object(formElement) {
+    const formData = new FormData(formElement) //key value
+    const data = {}
+    for (const [key, value] of formData) {
+        data[key] = value
+    }
+    console.log(data)
+    return data
+}
+
+async function addCensus(event) {
+    event.preventDefault() //do not reload the page
+
+    const newCensus = form2Object(formElement)
+
+    if (isEdit) { //editing
+        await db.collection('census').doc({id: censusToEditId}).update(newCensus)
+    } else { //adding
+        newCensus.id = Date.now().toString()
+        await db.collection('census').add(newCensus)
+    }
+
+    await showCensusData()
+    formElement.reset()
+}
 
 async function deleteCensus(cid) {
     await db.collection('census').doc({id: cid}).delete()
     await showCensusData()
+}
+
+async function editCensus(cid) {
+    const censusToEdit = await db.collection('census').doc({id: cid}).get()
+    countryInputBox.value = censusToEdit.country
+    populationInputBox.value = censusToEdit.population
+
+    censusToEditId = censusToEdit.id
+    isEdit = true
+
 }
 
 function censusToHTMLTRow(c) {
@@ -22,7 +60,7 @@ function censusToHTMLTRow(c) {
             <td>${c.country}</td>
             <td>${c.population}</td>
             <td>
-                <i class="fa fa-edit">Edit</i>
+                <i class="fa fa-edit"  onclick="editCensus('${c.id}')">Edit</i>
                 <i class="fa fa-trash" onclick="deleteCensus('${c.id}')">Delete</i>
             </td>
         </tr>
@@ -31,14 +69,14 @@ function censusToHTMLTRow(c) {
 
 async function showCensusData(noOfRows) {
     let census;
-    if(noOfRows)
-         census = await db.collection('census')
-             .limit(parseInt(noOfRows))
-             .orderBy("country", "asc")
-             .get()
+    if (noOfRows)
+        census = await db.collection('census')
+            .limit(parseInt(noOfRows))
+            .orderBy("country", "asc")
+            .get()
     else
-         census = await db.collection('census')
-             .orderBy("country", "asc").get()
+        census = await db.collection('census')
+            .orderBy("country", "asc").get()
 
     //we will convert this collection data into html table
     if (census.length > 0) {
@@ -56,18 +94,6 @@ async function showCensusData(noOfRows) {
     }
 }
 
-async function addCensus(event) {
-    event.preventDefault() //do not reload the page
-
-    const newCensus = {
-        id: Date.now().toString(),
-        country: countryInputBox.value,
-        population: populationInputBox.value
-    }
-    const message = await db.collection('census').add(newCensus)
-    await showCensusData()
-    formElement.reset()
-}
 
 
 
