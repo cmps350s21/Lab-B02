f8765025bd151ece0abeb161fad39f66fedf640c
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-03-25 16:47:18 +0300

    web api - get

--- Lab 9 Web API/BankingApp/app.js
+++ Lab 9 Web API/BankingApp/app.js
@@ -2,7 +2,29 @@ import express from 'express'
 const port = 3000
 const app = express() //server
 
-//dynamic data and static data
 app.use(express.static('public'))
+
+app.get('/api/greet', (req,res)=>{
+    const name = req.query.name
+    res.send(`<h1>Welcome Mr. ${req.query.name}</h1>`)
+})
+
+app.get('/api/accounts', (req, res)=>{
+    // const name = req.query
+    const accounts = [
+        {name : 'Abdulhi '} ,{name : 'Yousef'}
+    ]
+    res.json(accounts)
+})
+
 app.listen(port , ()=>{console.log(`Server Started Successfully @http://localhost:${port}`)})
 
+//CTRL + S
+// window user
+// npm i -g nodemon
+//
+// mac users
+// sudo npm i -g nodemon
+//
+// to run
+// nodemon
---
+++ Lab 9 Web API/BankingApp/repository/accounts-repo.js
@@ -0,0 +1,21 @@
+export default class AccountsRepo {
+    //crud operations
+
+    async addAccount(account){
+
+    }
+    async updateAccount(updatedAccount){
+
+    }
+    async getAccount(accountNo){
+
+    }
+    async deleteAccount(accountNo){
+
+    }
+    async getAccounts(type){
+
+    }
+}
+
+npm i fs-extra
